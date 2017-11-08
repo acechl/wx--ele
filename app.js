1a9eb39b5cc6e2be83d9a278944f1a357a2a25f9
acechl <1063256587@qq.com>
2017-11-08 10:17:42 +0800

    遮罩层 页面不能滚动

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
 //app.js
 let bmap = require("utils/bmap-wx.min");
+let Mock = require("./utils/mock");
 App({
    globalData:{
     userInfo:null,
     address: "未能获取地址...",
     winHeight: 0,
+    classify_left: {},
+    classify_right: {},
+    send_type: {},
+    shop_attr: {},
+    consume: {},
+    rank_type: {},
+    activity_type: {}
   },
   onLaunch () {
     //调用API从本地缓存中获取数据
@@ -45,6 +53,86 @@ App({
     }
   },
   onShow () {
+    console.log("nini")
+    let classify_left = Mock.mock({
+      "type": [
+          {"name":"全部商品","num":"9876"},
+          {"name":"美食","num":"654"},
+          {"name":"快餐便当","num":"234"},
+          {"name":"特色菜系","num":"234"},
+          {"name":"异国料理","num":"123"},
+          {"name":"小吃宵夜","num":"987"},
+          {"name":"甜品饮料","num":"435"},
+          {"name":"果蔬生鲜","num":"769"},
+          {"name":"商店超市","num":"432"},
+          {"name":"浪漫鲜花","num":"765"},
+          {"name":"医药健康","num":"342"},
+          {"name":"早餐","num":"987"},
+          {"name":"午餐","num":"564"},
+          {"name":"下午茶","num":"998"},
+          {"name":"晚餐","num":"887"},
+          {"name":"宵夜","num":"775"},
+      ]
+    });
+    let classify_right = Mock.mock({
+      "type": [
+        {"name":"全部","num":"8888"},
+        {"name":"简餐便当","num":"6666"},
+        {"name":"小吃炸串","num":"3333"},
+        {"name":"面食粥店","num":"2222"},
+        {"name":"地方菜系","num":"1111"},
+        {"name":"日韩料理","num":"888"},
+        {"name":"香锅冒菜","num":"666"},
+        {"name":"轻食西餐","num":"222"},
+        {"name":"汉堡披萨","num":"111"},
+      ]
+    });
+    let rank_type = Mock.mock({
+      "type": [
+        {"name":"综合排序"},
+        {"name":"销量最高"},
+        {"name":"起送价最低"},
+        {"name":"配送最快"},
+      ]
+    });
+    let send_type = Mock.mock({
+      "type": [
+        {"name": "蜂鸟专送","id":0}
+      ]
+    });
+    let consume = Mock.mock({
+      "type": [
+        {"name": "20以下","id":1},
+        {"name": "20-40","id":2},
+        {"name": "40以上","id":3},
+      ]
+    });
+    let activity_type = Mock.mock({
+      "type": [
+        {"title":"新","name":"新用户优惠","color":"rgb(112, 188, 70)","id":4},
+        {"title":"特","name":"特价商品","color":"rgb(241, 136, 79)","id":5},
+        {"title":"减","name":"下单立减","color":"rgb(240, 115, 115)","id":6},
+        {"title":"赠","name":"赠品优惠","color":"rgb(60, 199, 145)","id":7},
+        {"title":"返","name":"下单返券","color":"rgb(255, 114, 57)","id":8},
+        {"title":"领","name":"进店领劵","color":"rgb(255, 172, 42)","id":9},
+      ]
+    });
+    let shop_attr = Mock.mock({
+      "type": [
+        {"title":"品","name":"品牌商家","color":"rgb(63, 189, 230)","id":10},
+        {"title":"保","name":"外卖保","color":"rgb(153, 153, 153)","id":11},
+        {"title":"新","name":"新店","color":"rgb(232, 132, 45)","id":12},
+        {"title":"票","name":"开发票","color":"rgb(153, 153, 153)","id":13},
+        {"title":"付","name":"在线支付","color":"rgb(255, 78, 0)","id":14}
+      ]
+    });
+    this.globalData.classify_left = classify_left;
+    this.globalData.classify_right = classify_right;
+    this.globalData.rank_type = rank_type;
+    this.globalData.send_type = send_type;
+    this.globalData.consume = consume;
+    this.globalData.activity_type = activity_type;
+    this.globalData.shop_attr = shop_attr;
   }
    
 })
--- pages/menu/makeMenu/makeMenu.js
+++ pages/menu/makeMenu/makeMenu.js
@@ -8,10 +8,13 @@ Page({
         hot: {},
         history: {},
         search: false,
-        classify_left: {},
-        classify_right: {},
-        rank_type: {},
-        shop_attr: {},
+        classify_left: app.globalData.classify_left,
+        classify_right: app.globalData.classify_right,
+        rank_type: app.globalData.rank_type,
+        shop_attr: app.globalData.shop_attr,
+        consume: app.globalData.consume,
+        activity_type: app.globalData.activity_type,
+        send_type: app.globalData.send_type,
         show:{"classify":false,"rank":false,"selection":false},
         selection: [],
         winHeight: 0,
@@ -38,26 +41,13 @@ Page({
                 }
             ]
         })
-        let classify_left = Mock.mock({
-            "type": [
-                {"name":"全部商品","num":"9876"},
-                {"name":"美食","num":"654"},
-                {"name":"快餐便当","num":"234"},
-                {"name":"特色菜系","num":"234"},
-                {"name":"异国料理","num":"123"},
-                {"name":"小吃宵夜","num":"987"},
-                {"name":"甜品饮料","num":"435"},
-                {"name":"果蔬生鲜","num":"769"},
-                {"name":"商店超市","num":"432"},
-                {"name":"浪漫鲜花","num":"765"},
-                {"name":"医药健康","num":"342"},
-                {"name":"早餐","num":"987"},
-                {"name":"午餐","num":"564"},
-                {"name":"下午茶","num":"998"},
-                {"name":"晚餐","num":"887"},
-                {"name":"宵夜","num":"775"},
-            ]
-        });
+        let classify_left = this.data.classify_left;
+        let classify_right = this.data.classify_right;
+        let rank_type = this.data.rank_type;
+        let shop_attr = this.data.shop_attr;
+        let consume = this.data.consume;
+        let activity_type = this.data.activity_type;
+        let send_type = this.data.send_type;
         let selection = [];
         classify_left.type.forEach((value,index,arr)=>{
             if(index == 0) {
@@ -66,58 +56,6 @@ Page({
                 selection[index] = false
             }
         })
-        let classify_right = Mock.mock({
-            "type": [
-                {"name":"全部","num":"8888"},
-                {"name":"简餐便当","num":"6666"},
-                {"name":"小吃炸串","num":"3333"},
-                {"name":"面食粥店","num":"2222"},
-                {"name":"地方菜系","num":"1111"},
-                {"name":"日韩料理","num":"888"},
-                {"name":"香锅冒菜","num":"666"},
-                {"name":"轻食西餐","num":"222"},
-                {"name":"汉堡披萨","num":"111"},
-            ]
-        });
-        let rank_type = Mock.mock({
-            "type": [
-                {"name":"综合排序"},
-                {"name":"销量最高"},
-                {"name":"起送价最低"},
-                {"name":"配送最快"},
-            ]
-        });
-        let send_type = Mock.mock({
-            "type": [
-                {"name": "蜂鸟专送","id":0}
-            ]
-        });
-        let consume = Mock.mock({
-            "type": [
-                {"name": "20以下","id":1},
-                {"name": "20-40","id":2},
-                {"name": "40以上","id":3},
-            ]
-        });
-        let activity_type = Mock.mock({
-            "type": [
-                {"title":"新","name":"新用户优惠","color":"rgb(112, 188, 70)","id":4},
-                {"title":"特","name":"特价商品","color":"rgb(241, 136, 79)","id":5},
-                {"title":"减","name":"下单立减","color":"rgb(240, 115, 115)","id":6},
-                {"title":"赠","name":"赠品优惠","color":"rgb(60, 199, 145)","id":7},
-                {"title":"返","name":"下单返券","color":"rgb(255, 114, 57)","id":8},
-                {"title":"领","name":"进店领劵","color":"rgb(255, 172, 42)","id":9},
-            ]
-        });
-        let shop_attr = Mock.mock({
-            "type": [
-                {"title":"品","name":"品牌商家","color":"rgb(63, 189, 230)","id":10},
-                {"title":"保","name":"外卖保","color":"rgb(153, 153, 153)","id":11},
-                {"title":"新","name":"新店","color":"rgb(232, 132, 45)","id":12},
-                {"title":"票","name":"开发票","color":"rgb(153, 153, 153)","id":13},
-                {"title":"付","name":"在线支付","color":"rgb(255, 78, 0)","id":14}
-            ]
-        });
         let selected = {"send_type":[],"consume":[],"activity_type":[],"shop_attr":[]};
         send_type.type.forEach((value,index,arr)=>{
             selected.send_type[index] = undefined;
@@ -135,18 +73,11 @@ Page({
         this.setData({
             hot: hot,
             history: history,
-            classify_left: classify_left,
-            classify_right: classify_right,
-            rank_type: rank_type,
-            send_type: send_type,
-            consume: consume,
-            activity_type: activity_type,
-            shop_attr: shop_attr,
             selection: selection,
             winHeight: winHeight,
             selected: selected
         });
-        
+        console.log(this.data.winHeight)
     },
     onLaunch (options) {
         this.setData({
